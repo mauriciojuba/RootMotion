425a20a84310789c8264ac23db9757b444c0681f
mauriciojuba <mauriciojuba@outlook.com>
2016-04-11 05:09:21 -0300

    First Entry
    Import all the animations
    Walk and Run Foward/Backwards

---
+++ RPGCharController/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //turning
+        //jumping
+        Move();
+    }
+
+    void Move()
+    {
+        anim.SetFloat("Forward", Input.GetAxisRaw("Vertical"));
+    }
+}
