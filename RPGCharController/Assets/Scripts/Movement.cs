19c07c67d7b5754467fdc6b275cd99169a687e4d
mauriciojuba <mauriciojuba@outlook.com>
2016-04-13 12:05:42 -0300

    Look to mouse point implemented

--- RPGCharController/Assets/Scripts/Movement.cs
+++ RPGCharController/Assets/Scripts/Movement.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
     Animator anim;
+    Camera viewCamera;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+        viewCamera = Camera.main;
     }
 
     void Update()
@@ -15,6 +17,7 @@ public class Movement : MonoBehaviour {
         Turning();
         //jumping
         Move();
+        lookAt();
     }
 
     void Move()
@@ -24,6 +27,18 @@ public class Movement : MonoBehaviour {
     void Turning()
     {
         anim.SetFloat("Turn", Input.GetAxis("Horizontal"));
+    }
+    void lookAt()
+    {
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+        if(groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Vector3 heightCorrectedPoint = new Vector3(point.x, transform.position.y, point.z);
+            transform.LookAt(heightCorrectedPoint);
+        }
     }
 
 
