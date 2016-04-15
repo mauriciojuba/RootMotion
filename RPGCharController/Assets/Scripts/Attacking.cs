7cc05201260db9b117516500dce706d3d6e0ad8b
mauriciojuba <mauriciojuba@outlook.com>
2016-04-15 03:36:41 -0300

    15 abril
    Criei o Turning, que lida com o movimento dos pés quando o personagem
    olha para outra direção;

---
+++ RPGCharController/Assets/Scripts/Attacking.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacking : MonoBehaviour {
+
+
+    Animator anim;
+    public enum TypeOfAttack { No_Weapon, Melee_Weapon, Ranged_Weapon}
+    public TypeOfAttack typeofAttack;
+    public float range, damange;
+    void Start () {
+        typeofAttack = TypeOfAttack.No_Weapon;
+        anim = GetComponent<Animator>();
+	}
+	
+    void Update()
+    {
+        switch (typeofAttack)
+        {
+            case TypeOfAttack.No_Weapon:
+                range = 1f;
+                damange = 2f;
+                break;
+            case TypeOfAttack.Melee_Weapon:
+                range = 1f;
+                damange = 5f;
+                break;
+            case TypeOfAttack.Ranged_Weapon:
+                range = 3f;
+                damange = 10f;
+                break;
+        }
+    }
+
+    public void Attack()
+    {
+        anim.SetBool("isShooting", true);
+    }
+
+    public void ChangeToMMA()
+    {
+        typeofAttack = TypeOfAttack.No_Weapon;
+    }
+    public void ChangeToMelee()
+    {
+        typeofAttack = TypeOfAttack.Melee_Weapon;
+    }
+    public void ChangeToRanged()
+    {
+        typeofAttack = TypeOfAttack.Ranged_Weapon;
+    }
+}
--- RPGCharController/Assets/Scripts/Movement.cs
+++ RPGCharController/Assets/Scripts/Movement.cs
@@ -5,41 +5,96 @@ public class Movement : MonoBehaviour {
 
     Animator anim;
     Camera viewCamera;
+    Attacking attack;
+    public bool isMoving;
+    float newDirection, previousDirection;
+    float turnValue;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
         viewCamera = Camera.main;
+        attack = GetComponent<Attacking>();
+    }
+    void Start()
+    {
+        previousDirection = Mathf.Floor(transform.eulerAngles.y);
     }
 
     void Update()
     {
-        Turning();
+        lookAt();
         //jumping
         Move();
-        lookAt();
+        Turning();
+
+
+        //criar um script apenas para attack
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            attack.Attack();
+        }
+        else
+        {
+            anim.SetBool("isShooting", false);
+        }
+        //Nao esquecer
+
+
     }
+
+
+    void FixedUpdate()
+    {
+        newDirection = Mathf.Round(transform.eulerAngles.y);
+    }
+
+
 
     void Move()
     {
         anim.SetFloat("Forward", Input.GetAxis("Vertical"));
+        anim.SetFloat("Side", Input.GetAxis("Horizontal"));
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Horizontal") > 0)
+        {
+            isMoving = true;
+        }
     }
+
+
     void Turning()
     {
-        anim.SetFloat("Turn", Input.GetAxis("Horizontal"));
+        float turnvelocity = 0.01f;
+        if (previousDirection != newDirection)
+        {
+            if (previousDirection < newDirection - 1f)
+            {
+                turnValue = Mathf.SmoothDamp(turnValue, 0.8f, ref turnvelocity, 2 * Time.deltaTime);
+            }
+            else if (previousDirection > newDirection + 1f)
+            {
+                turnValue = Mathf.SmoothDamp(turnValue, -0.8f, ref turnvelocity, 2 * Time.deltaTime);
+            }
+        }
+        else if (turnValue != 0f)
+        {
+            turnValue = Mathf.SmoothDamp(turnValue, 0f, ref turnvelocity, 4 * Time.deltaTime);
+        }
+        anim.SetFloat("Turn", turnValue);
+        previousDirection = newDirection;
     }
+
+
     void lookAt()
     {
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayDistance;
-        if(groundPlane.Raycast(ray, out rayDistance))
+        if (groundPlane.Raycast(ray, out rayDistance))
         {
             Vector3 point = ray.GetPoint(rayDistance);
             Vector3 heightCorrectedPoint = new Vector3(point.x, transform.position.y, point.z);
             transform.LookAt(heightCorrectedPoint);
         }
     }
-
-
 }
